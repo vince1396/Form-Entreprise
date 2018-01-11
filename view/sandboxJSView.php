c4d9bb09a1f9f425f15d67e07cb6695e6179cd9e
draikan <31989340+draikan@users.noreply.github.com>
2018-01-11 13:45:41 +0100

    debut test js

---
+++ view/sandboxJSView.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-8 col-sm-6 col-md-6 col-xs-offset-2 col-sm-offset-3">
+            <form method="post">
+                <label>Zone 1</label>
+                <input type="text" name="zone1" id="zone1">
+                <label>Zone 2</label>
+                <input type="text" name="zone2" id="zone2">
+                <label>Zone 3</label>
+                <input type="text" name="zone3" id="zone3">
+                <label>Zone 4</label>
+                <input type="text" name="zone4" id="zone4">
+                <label>Zone 5</label>
+                <input type="text" name="zone5" id="zone5">
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+            </form>
+        </div>
+    </div>
+</div>
