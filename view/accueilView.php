ea65aed411bf4fd24f13013ece53d59d61074a63
FranckChaks <franck1891@hotmail.fr>
2018-01-11 14:54:00 +0100

    rectif repo

--- view/accueilView.php
+++ view/accueilView.php
@@ -1,28 +1,20 @@
 <div class="container">
     
     <section>
-        <div class="row">
-            <div class="col-xs-12 col-md-12">
-                <h2>Commercial</h2>
+        <div class="row accueil">
+            <div class="col-xs-12 col-md-4">
+               Hello
+            </div>
+        <div class="col-xs-12 col-md-4">
+
+            </div>
+       <div class="col-xs-12 col-md-4">
+               Hello
             </div>
         </div>
     </section>
     
-    <section>
-        <div class="row">
-            <form class="form-inline">
-  <div class="form-group">
-    <label for="nom">Nom</label>
-    <input type="text" class="form-control" id="exampleInputName2" placeholder="Jane Doe">
-  </div>
-  <div class="form-group">
-    <label for="prenom">Prénom</label>
-    <input type="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
-  </div>
-  <button type="submit" class="btn btn-default">Send invitation</button>
-</form>
-        </div>
-    </section>
+   
     
     
     
