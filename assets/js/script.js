e759c204512f539c7720aeed250f7118e7ed06c7
draikan <31989340+draikan@users.noreply.github.com>
2018-01-12 10:08:10 +0100

    useless cod

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,5 @@
 $(document).ready(function(){
 
-    $(".delete-flux").click(function() {
-        $("#modal-text").html("Voulez-vous supprimer ce flux " + $(this).html() + "?");
-        $("#yes").attr("href", "index.php?p=deleteflux&id=" + $(this).attr("id"));
-        $("#myalertbox").modal({
-            "keyboard": true,
-            "show": true
-        });
-    });
-    
-
-
-$("#ex1").slider();
-var mem = "<?php echo $r['credit'] ?>";
-
-// With JQuery
-    $("#ex1-enabled").click(function() {
-        if(this.checked) {
-            console.log(mem);
-		$("#ex1").slider("enable");
-            $('#ex1').slider({
-	formatter: function(value) {
-		return 'Current value: ' + value;
-	}
-});
-$("#ex1").on("slide", function(slideEvt) {
-	$("#ex1SliderVal").text(slideEvt.value);
-});
-	}
-	else {
-		$("#ex1").slider("disable");
-
-		
-	}
-});
     
     //hide and seek
 
