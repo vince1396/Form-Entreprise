ee9a8f3dd2d251fe1edf62763364e64801c6ca35
draikan <31989340+draikan@users.noreply.github.com>
2018-01-15 11:03:47 +0100

    star rating 1.2 fix done

--- template.php
+++ template.php
@@ -6,12 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-	<link rel="stylesheet" href="css/style.css"/>
-	<link rel="stylesheet" href="css/bootstrap.css"/>
-	<link rel="stylesheet" href="css/star-rating.min.css"/>
-	<link rel="stylesheet" href="css/star-rating.css"/>
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css"/>
+	<link rel="stylesheet" href="css/star-rating.css" type="text/css"/>
+   	<link rel="stylesheet" href="css/style.css" type="text/css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+    <!--  TEST  -->
+    
+    
+
+
+    <!--  FIN TEST  -->
+    
 </head>
 
 
--- view/footer.php
+++ view/footer.php
@@ -6,14 +6,6 @@
         </div>
     </div>
 </section>
-<!-- CORE JQUERY  -->
-<script src="assets/js/jquery-3.2.1.js"></script>
-<!-- BOOTSTRAP SCRIPTS  -->
-<script src="assets/js/bootstrap.js"></script>
-<script src="assets/js/bootstrap-fileupload.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.0.0/bootstrap-slider.js"></script>
-<!-- CUSTOM SCRIPTS  -->
-<script src="assets/js/script.js"></script>
     <!-- CORE JQUERY  -->
     <script src="assets/js/jquery-3.2.1.js"></script>
     <!-- BOOTSTRAP SCRIPTS  -->
