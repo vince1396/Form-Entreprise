2f4852a001585c049e79eb18268ce6c02961fc9d
draikan <31989340+draikan@users.noreply.github.com>
2018-01-15 11:36:59 +0100

    fix appels css

--- template.php
+++ template.php
@@ -8,12 +8,12 @@
     <meta name="author" content="">
     <!-- BOOTSTRAP STYLES-->
     <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/bootstrap.css" type="text/css"/>
+    <link rel="stylesheet" href="assets/css/bootstrap.css" type="text/css"/>
     <!-- FONTAWESOME STYLES-->
-    <link href="css/font-awesome.css" rel="stylesheet" />
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
     <!-- STAR RATING STYLES-->
-	<link rel="stylesheet" href="css/star-rating.css" type="text/css"/>
-   	<link rel="stylesheet" href="css/style.css" type="text/css"/>
+	<link rel="stylesheet" href="assets/css/star-rating.css" type="text/css"/>
+   	<link rel="stylesheet" href="assets/css/style.css" type="text/css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     
