ce86a470381aa5260633e7346426fb05a8d7f778
MarkoStefanovic-98 <markostef@hotmail.fr>
2018-01-15 10:44:58 +0100

    modif complète design de zero

--- template.php
+++ template.php
@@ -1,17 +1,23 @@
-<html>
+<html lang="fr">
 <head>
 	<title>Formulaire Allburotic</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 	<link rel="stylesheet" href="css/style.css"/>
 	<link rel="stylesheet" href="css/bootstrap.css"/>
 	<link rel="stylesheet" href="css/star-rating.min.css"/>
 	<link rel="stylesheet" href="css/star-rating.css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<meta charset="utf8"/>
+
+    <title>Formulaire Entreprise</title>
 </head>
 
 
-<body>
+<body id="page-top">
     <?php
        require "view/header.php";
        echo $content;
--- view/header.php
+++ view/header.php
@@ -1,15 +1,39 @@
-<div class="container-fluid">
-   <section>
-    <div class="row">
-        <div class="col-xs-12 col-md-12 logo">
-            <h1>LOGO</h1>
-               <a href="<?=BASE_URL;?>/ficheClient">FORMULAIRE CLIENT</a>
-               -----------------
-               <a href="<?=BASE_URL;?>/gestionAdmin">Gestion Admin</a>
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    <img src="../img/text4136.png" alt="" title="Formulaire"/>
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#melissa-finance">Accueil</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#nos-forces">A REMPLIR</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#entreprises">A REMPLIR</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#nos-solutions">A REMPLIR</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#nos-solutions">A REMPLIR</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#nos-solutions">A REMPLIR</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
         </div>
-    </div>
-    </section>
-    
- 
-    
-</div>
+        <!-- /.container-fluid -->
+    </nav>
