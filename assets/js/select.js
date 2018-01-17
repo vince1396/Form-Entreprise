e0af8f7961e260ac6d2a62f790abe5b0e103149f
draikan <31989340+draikan@users.noreply.github.com>
2018-01-17 12:14:51 +0100

    https://i.imgur.com/QMMtsAt.gif
    It's working It's working !
    (select finalisé)

--- assets/js/select.js
+++ assets/js/select.js
@@ -9,10 +9,50 @@ $(document).change(function(){
     var valTemp4 = $("#cInteret_4 option:selected").val();
     console.log("valeur du select 4 = "+valTemp4);
     
+    if ((valTemp !== '1' ) && (valTemp2 !== '1' ) && (valTemp3 !== '1') && (valTemp4 !== '1')){
+        $("#cInteret option[value='1']").removeAttr('disabled');
+        $("#cInteret_2 option[value='1']").removeAttr('disabled');
+        $("#cInteret_3 option[value='1']").removeAttr('disabled');
+        $("#cInteret_4 option[value='1']").removeAttr('disabled');
+    }
+    if ((valTemp !== '2' ) && (valTemp2 !== '2' ) && (valTemp3 !== '2') && (valTemp4 !== '2')){
+        $("#cInteret option[value='2']").removeAttr('disabled');
+        $("#cInteret_2 option[value='2']").removeAttr('disabled');
+        $("#cInteret_3 option[value='2']").removeAttr('disabled');
+        $("#cInteret_4 option[value='2']").removeAttr('disabled');
+    }
+    if ((valTemp !== '3' ) && (valTemp2 !== '3' ) && (valTemp3 !== '3') && (valTemp4 !== '3')){
+        $("#cInteret option[value='3']").removeAttr('disabled');
+        $("#cInteret_2 option[value='3']").removeAttr('disabled');
+        $("#cInteret_3 option[value='3']").removeAttr('disabled');
+        $("#cInteret_4 option[value='3']").removeAttr('disabled');
+    }
+    if ((valTemp !== '4' ) && (valTemp2 !== '4' ) && (valTemp3 !== '4') && (valTemp4 !== '4')){
+        $("#cInteret option[value='4']").removeAttr('disabled');
+        $("#cInteret_2 option[value='4']").removeAttr('disabled');
+        $("#cInteret_3 option[value='4']").removeAttr('disabled');
+        $("#cInteret_4 option[value='4']").removeAttr('disabled');
+    }
+    
     if (valTemp !== '0' ){
         $("#cInteret_2 option[value='"+valTemp+"']").attr('disabled','disabled');
         $("#cInteret_3 option[value='"+valTemp+"']").attr('disabled','disabled');
         $("#cInteret_4 option[value='"+valTemp+"']").attr('disabled','disabled');
+    }
+    if (valTemp2 !== '0' ){
+        $("#cInteret option[value='"+valTemp2+"']").attr('disabled','disabled');
+        $("#cInteret_3 option[value='"+valTemp2+"']").attr('disabled','disabled');
+        $("#cInteret_4 option[value='"+valTemp2+"']").attr('disabled','disabled');
+    }
+    if (valTemp3 !== '0' ){
+        $("#cInteret_2 option[value='"+valTemp3+"']").attr('disabled','disabled');
+        $("#cInteret option[value='"+valTemp3+"']").attr('disabled','disabled');
+        $("#cInteret_4 option[value='"+valTemp3+"']").attr('disabled','disabled');
+    }
+    if (valTemp4 !== '0' ){
+        $("#cInteret_2 option[value='"+valTemp4+"']").attr('disabled','disabled');
+        $("#cInteret_3 option[value='"+valTemp4+"']").attr('disabled','disabled');
+        $("#cInteret option[value='"+valTemp4+"']").attr('disabled','disabled');
     }
     
 });
