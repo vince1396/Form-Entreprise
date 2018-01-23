ba5498d50922da9fd39b85b907f4355a722178ee
vince1396 <vincent.cotini96@gmail.com>
2018-01-23 11:21:34 +0100

    admin manage user research bar

--- model/adminManageUser.php
+++ model/adminManageUser.php
@@ -1,3 +1,36 @@
 <?php
   //admin model
+
+  function getUsers()
+  {
+    $req = $bdd->prepare("SELECT * FROM user");
+    $req->execute();
+
+    return $req->fetch();
+  }
+
+  function getCommercial()
+  {
+    $req = $bdd->prepare("SELECT * FROM user WHERE lvl = 1");
+    $req->execute();
+
+    return $req->fetch();
+  }
+
+  function getChefVente()
+  {
+    $req = $bdd->prepare("SELECT * FROM user WHERE lvl = 2");
+    $req->execute();
+
+    return $req->fetch();
+  }
+
+  function ban($id_u)
+  {
+    $req = $bdd->prepare("UPDATE user SET lvl = 0 WHERE id_u = :id_u");
+    $req->bindValue(":id_u", $id_u, PDO::PARAM_INT);
+    $req->execute();
+
+    return $req->fetch();
+  }
 ?>
--- view/adminManageUser.php
+++ view/adminManageUser.php
@@ -1,3 +1,4 @@
-<?php
-  //admin view
-?>
+<form class="" action="#" method="post">
+  <input type="text" name="search">
+  <input type="submit" name="submit" name="Rechercher">
+</form>
