83a6205df305721186c375483c91f9ffde98c29d
vince1396 <vincent.cotini96@gmail.com>
2018-01-24 14:29:32 +0100

    .gitignore : ignorer la bdd

--- model/adminCreateUser.php
+++ model/adminCreateUser.php
@@ -1,22 +1,36 @@
 <?php
   function insertUser($nom, $prenom, $email, $mdp, $role, $chef)
   {
-    $req = $bdd->prepare("INSERT INTO user VALUES :nom, :prenom, :email, :mdp, :role, :chef");
-    $req->bindValue(":nom", $nom, PDO::PARAM_STR);
-    $req->bindValue(":prenom", $prenom, PDO::PARAM_STR);
-    $req->bindValue(":email", $email, PDO::PARAM_STR);
-    $req->bindValue(":mdp", $mdp, PDO::PARAM_STR);
-    $req->bindValue(":role", $role, PDO::PARAM_INT);
-    $req->bindValue(":chef", $chef, PDO::PARAM_INT);
-    $req->execute();
+      global $bdd;
+
+      $req = $bdd->prepare("INSERT INTO user VALUES :nom, :prenom, :email, :mdp, :role, :chef");
+      $req->bindValue(":nom", $nom, PDO::PARAM_STR);
+      $req->bindValue(":prenom", $prenom, PDO::PARAM_STR);
+      $req->bindValue(":email", $email, PDO::PARAM_STR);
+      $req->bindValue(":mdp", $mdp, PDO::PARAM_STR);
+      $req->bindValue(":role", $role, PDO::PARAM_INT);
+      $req->bindValue(":chef", $chef, PDO::PARAM_INT);
+      $req->execute();
   }
 
   function checkEmail($email)
   {
-    $req = $bdd->prepare("SELECT * FROM user WHERE :email = email");
-    $req->bindValue(":email", $email, PDO::PARAM_STR);
-    $req->execute();
+      global $bdd;
 
-    return $req->fetch();
+      $req = $bdd->prepare("SELECT * FROM user WHERE :email = email");
+      $req->bindValue(":email", $email, PDO::PARAM_STR);
+      $req->execute();
+
+      return $req->fetch();
+  }
+
+  function getChef()
+  {
+      global $bdd;
+
+      $req = $bdd->prepare("SELECT * FROM user WHERE lvl = 2");
+      $req->execute();
+
+      return $req->fetchAll();
   }
 ?>
