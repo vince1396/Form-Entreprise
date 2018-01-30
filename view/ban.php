9190e16155ec23defec115410c1bd6c7e56a9b2c
FranckChaks <franck1891@hotmail.fr>
2018-01-30 16:41:22 +0100

    Finalisation barre de recherche VERSION FINAL 4 REAL

---
+++ view/ban.php
@@ -0,0 +1,22 @@
+<div class="container white_body center">
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <br>
+    <br>
+    <h1>Vous n'êtes pas autorisé à accéder à ce contenu.</h1>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+
+
+</div>
