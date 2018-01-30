73444ce02e330cf711d44a4db258082278eaffdd
vince1396 <vincent.cotini96@gmail.com>
2018-01-30 16:33:14 +0100

    Update adminManageUser.php

--- view/adminManageUser.php
+++ view/adminManageUser.php
@@ -1,48 +1,34 @@
 <div class="container white_body">
  <form class="" action="#" method="post">
   <input type="text" name="search" placeholder="Rechercher un client">
-  <input type="submit" name="submit">
+  <input type="submit" name="search">
 </form>
-    <?php
-    if(isset($_POST['submit'])){ ?>
-        <table class="table">
-            <thead>
+    <table class="table">
+        <thead>
             <tr>
                 <th>#</th>
-                <th>Nom du commercial / chef</th>
-                <th>Nom du client</th>
-                <th>Option</th>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Email</th>
+                <th>Mot de passe</th>
+                <th>Options</th>
             </tr>
-            </thead>
-            <tbody>
+        </thead>
+        <tbody>
         <?php
-        foreach($req as $k=>$v){?>
+        foreach(getUsers() as $k=>$v){?>
             <tr>
-                <td>
-                    <?= $number; ?>
-                </td>
-                <td><span><?= $v["nom"]." ".$v['prenom'];?></span></td>
-                <td><span><?= $v["nom_c"];?></span></td>
-                <td><span>Consulter</span></td>
+                <td></td> <?= $number;     ?>   </td>
+                <td><span><?= $v["nom"];   ?>   </span></td>
+                <td><span><?= $v["prenom"];?>   </span></td>
+                <td><span><?= $v["email"]; ?>   </span></td>
+                <td><span>Crypté : uniquement modifiable</span></td>
+                <td><span><a href="index.php?update=<?= $v['id_u'] ?>">Modifier</a></span> /
+                    <span><a href="index.php?supp=<?= $v['id_u'] ?>">Supprimer</a></span></td>
             </tr>
 
         <?php
             $number++;}?>
-            </tbody>
-        </table>
-<?php } ?>
-<?php
-   foreach (getUsers() as $k => $v)
-   { ?>
-     <ul>
-      <li><?=$v['id_u']; ?></li>
-      <li><?=$v['nom']; ?></li>
-      <li><?=$v['prenom']; ?></li>
-      <li><?=$v['email']; ?></li>
-      <li><a href="index.php?supp=<?php echo $v['id_u'] ?>">Supprimer</a></li>
-    </ul>
-    <br>
-    <br>
-<?php }
-?>
+        </tbody>
+    </table>
 </div>
