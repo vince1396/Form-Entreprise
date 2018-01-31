06ec668dad91a703958135af38f3e95128f53fac
vince1396 <vincent.cotini96@gmail.com>
2018-01-31 10:58:06 +0100

    Correction index

--- index.php
+++ index.php
@@ -1,40 +1,46 @@
 <?php
   session_start();
 
-  require "core/functions.php";
-  require "model/bdd.php";
+require "core/functions.php";
+require "model/bdd.php";
 
-  define('WEBROOT', dirname(__FILE__));
-  define('BASE_URL', dirname($_SERVER['SCRIPT_NAME']));
-  define('ROOT', dirname(WEBROOT));
-  define('DS', DIRECTORY_SEPARATOR);
-  define('CORE',ROOT.DS.'core');
+define('WEBROOT', dirname(__FILE__));
+define('BASE_URL', dirname($_SERVER['SCRIPT_NAME']));
+define('ROOT', dirname(WEBROOT));
+define('DS', DIRECTORY_SEPARATOR);
+define('CORE',ROOT.DS.'core');
 
-  if (isset($_SESSION) AND $_SESSION['lvl'] == 0) {
-
+if (isset($_SESSION))
+{
+    if($_SESSION['lvl'] == 0)
+    {
         $page = "ban";
     }
-
-  if(!isset($_GET['p']) || $_GET['p'] == "")
-  {
-    $page = $_GET["p"] = 'login';
-  }
-  else
-  {
-    if(!file_exists("controller/".$_GET['p'].".php"))
+}
+else
+{
+    if(!isset($_GET['p']) || $_GET['p'] == "")
     {
-      $page = $_GET['p'] = '404';
+        $page = $_GET["p"] = 'login';
     }
     else
     {
-      $page = $_GET['p'];
+        if(!file_exists("controller/".$_GET['p'].".php"))
+        {
+            $page = $_GET['p'] = '404';
+        }
+        else
+        {
+            $page = $_GET['p'];
+        }
     }
-  }
+}
 
-  ob_start();//permet de ne plus renvoyer de contenu au navigateur
+
+ob_start();//permet de ne plus renvoyer de contenu au navigateur
     require "controller/".$page.".php";
     $content = ob_get_contents();//permet de recuperer le contenu executé depuis ob_start
-  ob_end_clean();
+ob_end_clean();
 
-  require "template.php"
+require "template.php"
 ?>
