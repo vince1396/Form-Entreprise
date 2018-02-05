91e20d4bf1fc847fa88662512f184998195ead6d
FranckChaks <franck1891@hotmail.fr>
2018-02-05 11:16:34 +0100

    Ajout logo espace admin

--- view/adminMainView.php
+++ view/adminMainView.php
@@ -7,6 +7,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 col-xs-12 col-lg-12 text-center">
+                <img src="./assets/css/logo_admin.png" class="logo-accueil">
             </div>
             <div class="container-fluid">
                 <div class="row popup-gallery">
