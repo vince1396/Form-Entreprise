55b69c8d0de93fa645aa50f07dbc2cae0ff37a88
FranckChaks <franck1891@hotmail.fr>
2018-02-06 14:33:08 +0100

    fonction recherche espace chef de vente

--- controller/gestionChef.php
+++ controller/gestionChef.php
@@ -4,7 +4,72 @@
     
     if($_SESSION['lvl'] == 2){
         
-        $req = displayCommerciaux();
+        $r = displayCommerciaux();
+
+        $message = '';
+
+        $sql = "SELECT DISTINCT u.nom, u.prenom, f.id_f, c.id_c, c.raison, c.nom_c, f.date_rdv, c.interet, f.prise_rdv, DATE_FORMAT(f.date_rdv, '%d/%m/%Y') as date_rdv FROM user u, fiche f, client c, projet p WHERE u.id_chef =".$_SESSION['id']." AND f.id_u = u.id_u AND f.id_f = c.id_f ";
+
+        if(isset($_POST['submit'])) {
+
+            $i = 0;
+            if (empty($_POST['interet']) && empty($_POST['cInteret']) && empty($_POST['prospect']) && empty($_POST['rechercher']) && empty($_POST['date_min']) && empty($_POST['date_max'])) {
+                $message = "Veuillez séléctionner des critères valides.";
+            } else {
+                if (isset($_POST['interet'])) {     //Niveau d'intérêt
+                    $interet = $_POST['interet'];
+                    if ($interet == 1) {
+                        $sql .= "AND (c.interet = 1)";
+                    }
+                    if ($interet == 2) {
+                        $sql .= "AND (c.interet = 2)";
+                    }
+                    if ($interet == 3) {
+                        $sql .= "AND (c.interet = 3)";
+                        $i++;
+                    }
+                }
+//Critères de recherche selon le type de contrat (1= Bureautique, 2=Info, 3=Solution & 4=Téléphonie)
+                if (isset($_POST['cInteret'])) {
+                    if ($_POST['cInteret'] == 1) {
+                        $sql .= "AND c.id_c = p.id_c AND p.id_p IN (SELECT b.id_b FROM bureautique b, projet p, client c WHERE b.id_b = p.id_p AND p.id_c = c.id_c)";
+
+                    }
+                    if ($_POST['cInteret'] == 2) {
+                        $sql .= "AND c.id_c = p.id_c AND  p.id_p IN (SELECT i.id_i FROM informatique i, projet p, client c WHERE i.id_i = p.id_p AND p.id_c = c.id_c)";
+                    }
+                    if ($_POST['cInteret'] == 3) {
+                        $sql .= "AND c.id_c = p.id_c AND p.id_p IN (SELECT s.id_s FROM solution s, projet p, client c WHERE s.id_s = p.id_p AND p.id_c = c.id_c)";
+                    }
+                    if ($_POST['cInteret'] == 4) {
+                        $sql .= "AND c.id_c = p.id_c AND p.id_p IN (SELECT t.id_t FROM telephonie t, projet p, client c WHERE t.id_t = p.id_p AND p.id_c = c.id_c)";
+                    }
+                }
+                if (isset($_POST['prospect'])) {    //Critère de recherche si c'est un prospect ou un client
+                    if ($_POST['prospect'] == 1) {
+                        $sql .= "AND c.prospect = 1 ";
+                    }
+                    if ($_POST['prospect'] == 2) {
+                        $sql .= "AND c.prospect = 2 ";
+                    }
+                }
+                if (!empty($_POST['rechercher']) && isset($_POST['rechercher'])) {  //Barre de recherche -> A optimiser car trop lent
+                    $rechercher = $_POST['rechercher'];
+                    $sql .= "AND ((c.nom_c LIKE '%" . $rechercher . "%') OR (u.nom LIKE '%" . $rechercher . "%') OR (u.prenom LIKE '%" . $rechercher . "%') OR (c.mail_c LIKE '%" . $rechercher . "%') OR (c.ville LIKE '%" . $rechercher . "%') OR (c.cp LIKE '%" . $rechercher . "%') OR (c.tel LIKE '%" . $rechercher . "%') OR (c.fax LIKE '%" . $rechercher . "%') OR (c.raison LIKE '%" . $rechercher . "%'))";
+                    $i++;
+                }
+                if (isset($_POST['date_min']) && ($_POST['date_max'])) {    //Recherche par intervalle de date
+                    $date_min = $_POST['date_min'];
+                    $date_max = $_POST['date_max'];
+                    $sql .= " AND f.date_rdv >= '" . $date_min . "' AND f.date_rdv <= '" . $date_max . "' ORDER BY date_rdv";
+
+                }
+//                var_dump($sql);
+//                die();
+                $req = $bdd->query($sql);
+
+            }
+        }
     }
     else
         header("location:accueil");
--- view/gestionChefView.php
+++ view/gestionChefView.php
@@ -1,4 +1,96 @@
 <div class="white_body container">
+    <tbody class="col-md-12">
+    <div class="search_form">
+        <form action="#" method="post">
+            <select name="interet" class="select_interet">
+                <option value="0">Niveau d'intérêt</option>
+                <option value="1">*</option>
+                <option value="2">**</option>
+                <option value="3">***</option>
+            </select>
+            <select name="prospect" class="select_prospect">
+                <option value="0">Séléctionner un type</option>
+                <option value="1">Prospect</option>
+                <option value="2">Client</option>
+            </select>
+            <select name="cInteret" class="select_cinteret">
+                <option value="0">Type de contrat</option>
+                <option value="1"  class="cInteret1">Bureautique</option>
+                <option value="2"  class="cInteret2">Informatique</option>
+                <option value="3"  class="cInteret3">Solution</option>
+                <option value="4"  class="cInteret4">Téléphonie</option>
+            </select>
+
+            <!-------------------------------------------->
+
+            <label for="date_min" class="label_date">Entre le:
+                <input type="date" name="date_min"></label>
+            <label for="date_max" class="label_date">Et le:
+                <input type="date" name="date_max"></label>
+            <label for="rechercher" class="label_search">
+                <input type="text" name="rechercher" placeholder="Rechercher..."  class="search">
+                <input type="submit" name="submit" class="submit_search"></label>
+        </form>
+    </div>
+
+    <?php
+    if(isset($_POST['submit'])) {
+    $temoin = true;
+    $number = 1;
+    if ($message != "") {
+        echo "<b>".$message."</b>";
+    } else { ?>
+    <br>
+    <h3>Résultats de la recherche:</h3>
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered table-hover table_historique">
+            <thead>
+            <tr>
+                <th class="th_search">#</th>
+                <th class="th_search">Commercial</th>
+                <th class="th_search">Raison sociale</th>
+                <th class="th_search">Nom du client</th>
+                <th class="th_search">Date de rendez-vous</th>
+                <th class="th_search">Option</th>
+                <th class="th_search">Niveau d'intérêt</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach ($req as $k => $v) { ?>
+                <tr>
+                    <td>
+                        <?= $number; ?>
+                    </td>
+                    <td><span><?= $v["nom"].' '.$v["prenom"]; ?></span></td>
+                    <td><span><?= $v["prise_rdv"]; ?></span></td>
+                    <td><span><?= $v["nom_c"]; ?></span></td>
+                    <td><?= $v['date_rdv']; ?></td>
+                    <td><span><a href="index.php?p=consulter&id=<?= $v["id_f"]; ?>">Voir le dossier</a></span>
+                    </td>
+                    <td><?= $v['interet']; ?></td>
+                </tr>
+                <?php $number++; ?>
+
+                <?php $temoin = false; ?>
+
+            <?php }
+            if ($temoin) { ?>
+                <tr>
+                    <td>Aucun résultat</td>
+                    <td>Aucun résultat</td>
+                    <td>Aucun résultat</td>
+                    <td>Aucun résultat</td>
+                    <td>Aucun résultat</td>
+                    <td>Aucun résultat</td>
+                </tr>
+                <?php
+            }?>
+            </tbody>
+        </table>
+    </div>
+    <?php }
+                }?>
                <h2 class="center">Liste des commerciaux</h2>
                 <div class="table-responsive">
                     <table class="table table-striped table-bordered table-hover">
@@ -13,7 +105,7 @@
                         <tbody>
                             <?php
                                 $number = 1 ;
-                                foreach($req as $k=>$v){
+                                foreach($r as $k=>$v){
                             ?>
                                 <tr>
                                     <td>
